074ff1a03acc9eeff4ae0dda9f1272e614b686af
Fahim Islam <160403765+akibul123@users.noreply.github.com>
2024-10-12 14:43:35 +0600

    Update 00couple.js

--- scripts/commands/00couple.js
+++ scripts/commands/00couple.js
@@ -1,10 +1,15 @@
+/**
+* @author ProCoderMew
+* @warn Do not edit code or edit credits
+*/
+
 module.exports.config = {
     name: "couple",
     version: "2.0.0",
     hasPermssion: 0,
-    credits: "MrTomXxX",
-    description: "Seo phi",
-    commandCategory: "Love",
+    credits: "DinhPhuc",
+    description: "",
+    commandCategory: "couple",
     usages: "[tag]",
     cooldowns: 5,
     dependencies: {
@@ -19,10 +24,11 @@ module.exports.onLoad = async() => {
     const { resolve } = global.nodemodule["path"];
     const { existsSync, mkdirSync } = global.nodemodule["fs-extra"];
     const { downloadFile } = global.utils;
-    const dirMaterial = __dirname + `/cache/canvas/`;
-    const path = resolve(__dirname, 'cache/canvas', 'seophi.png');
-    if (!existsSync(dirMaterial + "canvas")) mkdirSync(dirMaterial, { recursive: true });
-    if (!existsSync(path)) await downloadFile("https://i.imgur.com/hmKmmam.jpg", path);
+    const dirMaterial = __dirname + `/cache/`;
+    const path = resolve(__dirname, 'cache', 'https://i.imgur.com/hmKmmam.jpg.png');
+    if (!existsSync(dirMaterial + "")) mkdirSync(dirMaterial, { recursive: true });
+    if (!existsSync(path)) await downloadFile("https://scontent.xx.fbcdn.net/v/t1.15752-9/457052121_2452725664929010_8545876069089647479_n.jpg?stp=dst-jpg_p480x480&_nc_cat=109&ccb=1-7&_nc_sid=9f807c&_nc_eui2=AeFmpHJvZzyudrvH1Om5BVxak_o0ET8RsuqT-jQRPxGy6pd15Ay-d7kVLoVp-DUXYL5x6f4bsGWgwTX72S-ZuXxx&_nc_ohc=GQ96beK0vrsQ7kNvgHOGtPN&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&_nc_gid=A3rySk6anxu3dfmDAwnHWcL&oh=03_Q7cD1QGYvVgax1rRNrstS6NHNkeA6k8GmQqgn3LkDF1Dm_Vo_w&oe=67023F1D", path);
+
 }
 
 async function makeImage({ one, two }) {
@@ -30,10 +36,10 @@ async function makeImage({ one, two }) {
     const path = global.nodemodule["path"];
     const axios = global.nodemodule["axios"]; 
     const jimp = global.nodemodule["jimp"];
-    const __root = path.resolve(__dirname, "cache", "canvas");
+    const __root = path.resolve(__dirname, "cache");
 
-    let batgiam_img = await jimp.read(__root + "/seophi.png");
-    let pathImg = __root + `/batman${one}_${two}.png`;
+    let hon_img = await jimp.read(__root + "/Raja2.png");
+    let pathImg = __root + `/Raja2_${one}_${two}.png`;
     let avatarOne = __root + `/avt_${one}.png`;
     let avatarTwo = __root + `/avt_${two}.png`;
     
@@ -45,9 +51,10 @@ async function makeImage({ one, two }) {
     
     let circleOne = await jimp.read(await circle(avatarOne));
     let circleTwo = await jimp.read(await circle(avatarTwo));
-    batgiam_img.resize(1024, 712).composite(circleOne.resize(200, 200), 527, 141).composite(circleTwo.resize(200, 200), 389, 407);
+    hon_img.resize(719, 405).composite(circleOne.resize(150, 150), 515, 107
+ ).composite(circleTwo.resize(150, 150), 54, 105);
     
-    let raw = await batgiam_img.getBufferAsync("image/png");
+    let raw = await hon_img.getBufferAsync("image/png");
     
     fs.writeFileSync(pathImg, raw);
     fs.unlinkSync(avatarOne);
@@ -67,14 +74,14 @@ module.exports.run = async function ({ event, api, args }) {
     const { threadID, messageID, senderID } = event;
     var mention = Object.keys(event.mentions)[0]
     let tag = event.mentions[mention].replace("@", "");
-    if (!mention) return api.sendMessage("Vui lòng tag 1 người", threadID, messageID);
+    if (!mention) return api.sendMessage("Please tag 1 person", threadID, messageID);
     else {
         var one = senderID, two = mention;
-        return makeImage({ one, two }).then(path => api.sendMessage({ body: "Ship ",
+        return makeImage({ one, two }).then(path => api.sendMessage({ body: "This "  +  tag + ' love you so muchÃ°Å¸â€™â€',
             mentions: [{
           tag: tag,
           id: mention
         }],
      attachment: fs.createReadStream(path) }, threadID, () => fs.unlinkSync(path), messageID));
     }
-}
+                    }
