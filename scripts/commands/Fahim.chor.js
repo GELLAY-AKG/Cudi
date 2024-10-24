961613dac1d2f4ab413d99bd9c886a7552875d47
GELLAY-AKG <Akibulislam123456788@gmail.com>
2024-10-24 10:34:02 +0600

    Create Fahim.chor.js

---
+++ scripts/commands/Fahim.chor.js
@@ -0,0 +1,63 @@
+module.exports.config = {
+  name: "chor", 
+  version: "1.0.0", 
+  permission: 0,
+  credits: "Md Fahim Islam",
+  description: "",
+  prefix: true,
+  category: "murgi chor", 
+  usages: "user", 
+  cooldowns: 5,
+  dependencies: {
+        "axios": "",
+        "fs-extra": "",
+        "path": "",
+        "jimp": ""
+    }
+};
+
+module.exports.circle = async (image) => {
+	  const jimp = global.nodemodule['jimp'];
+  	image = await jimp.read(image);
+  	image.circle();
+  	return await image.getBufferAsync("image/png");
+};
+
+module.exports.run = async ({ event, api, args, Users }) => {
+try {
+  const Canvas = global.nodemodule['canvas'];
+  const request = global.nodemodule["node-superfetch"];
+  const jimp = global.nodemodule["jimp"];
+  const fs = global.nodemodule["fs-extra"];
+  var path_toilet = __dirname+'/cache/damma.jpg'; 
+  var id = Object.keys(event.mentions)[0] || event.senderID;
+  const canvas = Canvas.createCanvas(500, 670);
+	const ctx = canvas.getContext('2d');
+	const background = await Canvas.loadImage('https://i.imgur.com/ES28alv.png');
+  
+	var avatar = await request.get(`https://graph.facebook.com/${id}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`);
+	avatar = await this.circle(avatar.body);
+	ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+	ctx.drawImage(await Canvas.loadImage(avatar), 48, 410, 111, 111);
+	const imageBuffer = canvas.toBuffer();
+	fs.writeFileSync(path_toilet,imageBuffer);
+	 api.sendMessage({attachment: fs.createReadStream(path_toilet, {'highWaterMark': 128 * 1024}), body: "মুরগির ডিম চুরি করতে গিয়া ধরা থাইসে 🐸👻"}, event.threadID, () => fs.unlinkSync(path_toilet), event.messageID);
+}
+catch(e) {api.sendMessage(e.stack, event.threadID )}
+  }
+  module.exports.run = async function ({ event, api, args }) {
+    const fs = global.nodemodule["fs-extra"];
+    const { threadID, messageID, senderID } = event;
+    var mention = Object.keys(event.mentions)[0]
+    let tag = event.mentions[mention].replace("@", "");
+    if (!mention) return api.sendMessage("Vui lòng tag 1 người", threadID, messageID);
+    else {
+        var one = senderID, two = mention;
+        return makeImage({ one, two }).then(path => api.sendMessage({ body: "kiss ",
+            mentions: [{
+          tag: tag,
+          id: mention
+        }],
+     attachment: fs.createReadStream(path) }, threadID, () => fs.unlinkSync(path), messageID));
+    }
+}
